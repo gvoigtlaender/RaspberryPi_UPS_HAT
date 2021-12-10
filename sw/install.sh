71dd852487a200889147a646dfd343f74c6c1981
Georg Voigtländer <gvoigtlaender@googlemail.com>
2021-12-10 08:49:23 +0100

    update: logs, install python3-smbus

--- sw/install.sh
+++ sw/install.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
+echo copying ups.service > /etc/systemd/system/
 cp ups.service /etc/systemd/system/
+
+echo ups.py /opt/
 cp ups.py /opt/
+
+echo installing python3-smbus 
+apt install python3-smbus
+
+echo enabling ups.service
 systemctl enable ups.service
+
+echo starting ups.service
 systemctl start ups.service
